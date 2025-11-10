0c0bea74e2c3d604e064ebc0830b32e548705112
pasechnik8 <samorukov2006@mail.ru>
2025-11-10 23:17:43 +0700

    creating basic files

---
+++ server/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TeamTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сервер командного трекера запущен!");
+        }
+    }
+}
