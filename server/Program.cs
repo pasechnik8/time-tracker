a9ccdce0b80b98d48f481af0c005e795fd0716f3
pasechnik8 <samorukov2006@mail.ru>
2025-11-27 22:01:26 +0700

    creatind BD SQLite

--- server/Program.cs
+++ server/Program.cs
@@ -8,9 +8,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("TeamTrackerDB"));
+    options.UseSqlite("Data Source=teamtracker.db"));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI(options =>
@@ -23,6 +29,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-app.MapGet("/", () => "Сервер командного трекера запущен");
+app.MapGet("/", () => "командный трекер часов");
 
 app.Run();
