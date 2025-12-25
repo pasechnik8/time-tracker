db7d8fea45a0e8c2bedd61c1ec35d7960143080e
pasechnik8 <samorukov2006@mail.ru>
2025-12-26 01:04:49 +0700

    add new migration

---
+++ server/Migrations/20251225180318_UpdateSubjectModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace timetracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSubjectModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
